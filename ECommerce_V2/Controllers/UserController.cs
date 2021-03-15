0f3ef54544cd3c4c4fec3ab7457df8da23980455
ahmad_sabry_28 <ahmadsabry28@hotmail.com>
2021-03-15 05:44:01 +0200

    #commit controller

--- ECommerce_V2/Controllers/UserController.cs
+++ ECommerce_V2/Controllers/UserController.cs
@@ -1,31 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using V2.DAL.DB_Context;
-using V2.DAL.Models;
 
 namespace V2.Website.Controllers
 {
     public class UserController : Controller
     {
 
-        private readonly UnitOfWork unitOfWork;
-
-
         public IActionResult Index()
         {
             return View();
         }
 
-        // GET: /user/Details/5
-        public ViewResult Details(int id)
-        {
-            User user = unitOfWork.Users.FindById(id);
-            return View(user);
-        }
 
-        // GET: /user/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
     }
 }
